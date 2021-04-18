d33a1c6979bc3e32a560d8234694fec74487d8ac
Ferran <ferran.puigmar@gmail.com>
2021-04-18 23:35:15 +0200

    Añadido webpack para general el codigo css y js

---
+++ Assets/js/bundle.js
@@ -0,0 +1,6 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+const hello = saludo => saludo;
+
+/******/ })()
+;
---
+++ dist/webpack.config.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: './Assets/js'
+  },
+  mode: 'none'
+};
