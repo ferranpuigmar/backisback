7dcb9420d946c756791fb3d8dd6374e1268b15aa
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-02 01:10:41 +0200

    Update registerModel.php

--- Models/registerModel.php
+++ Models/registerModel.php
@@ -27,8 +27,7 @@ class registerModel extends Mysql
     //ojo ver con recoger la fecha y hora en formato timestamp $this->???Date_registered= $date_registered;
          
          $sql = "INSERT INTO students(username,pass,email,name,surname,telephone,nif,date_registered) VALUES(?,?,?,?,?,?,?,?)";
-        // $insert = $this->conexion->prepare($sql);
-		 $arrData = array($this->strUsername,$this->strPassword,$this->strEmail,$this->strEmail,$this->strName,$this->strSurname,$this->strTelephone,$this->strNif);
+         $arrData = array($this->strUsername,$this->strPassword,$this->strEmail,$this->strEmail,$this->strName,$this->strSurname,$this->strTelephone,$this->strNif);
          $request_insert = $this->insert($sql,$arrData);
          return $request_insert;
 
