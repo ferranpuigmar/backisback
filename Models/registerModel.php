5de3e615e7d0081a31de0690f3964ad247b98bbd
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-01 23:55:57 +0200

    Update registerModel.php

--- Models/registerModel.php
+++ Models/registerModel.php
@@ -6,4 +6,23 @@ class registerModel extends Mysql
     {
         parent::__construct();
     }
+     public function registerStudent(string $username, string $pass, string $email, string $name, string $surname, string $telephone, string $nif)
+    {
+        $this->strUsername = $username;
+        $this->strPassword = $pass;
+        $this->strEmail = $email;
+        $this->strName = $name;
+        $this->strSurname = $surname;
+        $this->strTelephone = $telephone;
+        $this->strNif = $nif;
+    //ver con recoger la fecha y hora en formato timestamp $this->???Date_registered= $date_registered;
+         
+
+         $sql = "INSERT INTO students(username, pass, email, name, surname, telephone, nif, date_registered) 
+                 VALUES (?,?,?,?,?,?,?,?)";
+         
+
+        $request = $this->select($sql);
+        return $request;
+    }
 }
