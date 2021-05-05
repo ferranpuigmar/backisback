527611f8e85506c2b36fe1a675d40eb8aa281bf3
Ferran <ferran.puigmar@gmail.com>
2021-05-06 00:20:45 +0200

    template dashboard

--- Views/Components/header.php
+++ Views/Components/header.php
@@ -1,23 +1,23 @@
 <header class="dashboardHeader">
   <div class="dashboardHeader__wrapper">
     <div class="dashboardHeader__logo">
-      LOGO
+      Calendario Académico
     </div>
     <div class="dashboardHeader__userActions">
       <div class="dashboardHeader__user">
         <div id="avatar">
           <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="64px" height="64px" viewBox="0 0 64 64" version="1.1">
-            <circle fill="#ff0000" width="64" height="64" cx="32" cy="32" r="32" /><text x="50%" y="50%" style="color: #ffffff;line-height: 1;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;" alignment-baseline="middle" text-anchor="middle" font-size="26" font-weight="400" dy=".1em" dominant-baseline="middle" fill="#ffffff">AB</text>
+            <circle fill="#4681e1a7" width="64" height="64" cx="32" cy="32" r="32" /><text x="50%" y="50%" style="color: #ffffff;line-height: 1;font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;" alignment-baseline="middle" text-anchor="middle" font-size="26" font-weight="400" dy=".1em" dominant-baseline="middle" fill="#ffffff">AB</text>
           </svg>
         </div>
         <div class="dashboardHeader__user__info">
           <div class="dashboardHeader__user__info__username">
             <div class="dashboardHeader__user__info__name">Hola Juanjo</div>
-            <a href="">Editar datos</a>
+            <a href="#">Editar datos</a>
           </div>
           <div class="dashboardHeader__user__info__logout">
             <i class="far fa-times-circle"></i>
-            <a href="">Cerrar sesión</a>
+            <a href="#">Cerrar sesión</a>
           </div>
         </div>
       </div>
--- Views/Templates/dashboard_template.php
+++ Views/Templates/dashboard_template.php
@@ -4,16 +4,45 @@ include "Views/components/head.php";
 <?php
 include "Views/components/header.php";
 ?>
-<main template="dashboard_template">
-  <div class="container">
-    <div class="dashboardSection">
-      <sidebar>
-        menu...
-      </sidebar>
-      <article>
-        <?php include $view ?>
-      </article>
-    </div>
+<main class="dashboard_template">
+  <div class="dashboardSection">
+    <sidebar>
+      <ul>
+        <li>
+          <div>
+            <i class="far fa-calendar-alt"></i>
+            <a href="#">Calendario</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <i class="fas fa-chalkboard"></i>
+            <a href="#">Gestionar clases</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <i class="fas fa-book-open"></i>
+            <a href="#">Gestionar curso</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <i class="fas fa-user-cog"></i>
+            <a href="#">Asignar Roles</a>
+          </div>
+        </li>
+        <li>
+          <div>
+            <i class="fas fa-chalkboard-teacher"></i>
+            <a href="#">Gestionar profesores</a>
+          </div>
+        </li>
+      </ul>
+    </sidebar>
+    <article>
+      <?php include $view ?>
+    </article>
   </div>
 </main>
 <?php
