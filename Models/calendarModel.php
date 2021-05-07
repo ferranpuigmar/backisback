1db8312110368233b2efdaf553392fbba4d6cc25
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-07 19:28:55 +0200

    Update calendarModel.php

--- Models/calendarModel.php
+++ Models/calendarModel.php
@@ -2,8 +2,26 @@
 
 class calendarModel extends Mysql
 {
+    private $strUser;
+    private $strPassword;
+  
   public function __construct()
   {
     parent::__construct();
   }
+   //recuperamos calendario de clases de un estudiante
+
+    public function listSchedule()
+    { 
+        $sql = "select en.id_course, cla.id_class, sch.day, sch.time_start, sch.time_end, concat_ws('  ', te.name, te.surname)
+                from students s JOIN enrollment en JOIN class cla JOIN schedule sch JOIN teachers te
+                where s.username     = '$this->strUser' and
+                      s.pass         = '$this->strPassword'
+                      s.id           = en.id_student AND 
+                      en.id_course   = cla.id_course and 
+                      cla.id_class   = sch.id_class and
+                      cla.id_teacher = te.id_teacher";
+        $request = $this->select_all($sql);
+        return $request;      
+    }
 }
