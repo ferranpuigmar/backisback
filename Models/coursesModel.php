2aac8a835ed1186b9bf932c436e8ddf0d65c8ad7
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-09 19:31:41 +0200

    Update coursesModel.php

--- Models/coursesModel.php
+++ Models/coursesModel.php
@@ -1,11 +1,8 @@
 <?php
 
-class cursosModel extends Mysql
+class coursesModel extends Mysql
 {
-    private $intIdUser;
-    private $strUser;
    
-
     public function __construct()
     {
         parent::__construct();
@@ -14,23 +11,24 @@ class cursosModel extends Mysql
     public function listCourses()
     {
         $sql = "SELECT * from courses";
-        $requestUser = $this->select all($sql);  
-           return $requestCourses;
+        $reslistCourses = $this->select all($sql);  
+           return $reslistCourses;
     }
      // ACTUALIZAMOS ALGUNO DE LOS CAMPOS DE CURSOS 
   
     public function updateCourses(int $id_course, string $name,string $description,string $date_start,string $date_end, string $active)
     {
         $this->strName = $name;
-			  $this->strDescription = $description;
-			  $this->strDate_start = $date_start;
+	$this->strDescription = $description;
+	$this->strDate_start = $date_start;
         $this->strDate_end = $date_end;
         $this->strActive = $active; 
       
         $sql = "UPDATE courses SET name=?, description=?, date_start=?, date_end=?, active=? WHERE id=$id_course ";
-		  	$arrData = array($this->strNombre,$this->intTelefono,$this->strEmail);
-	  		$resExecute = $update->execute($arrData);
-		  	return $resExecute;
+        $arrData = array($this->strName,$this->srtDescription,$this->strDate_start,$this->srtDate_end,$this->strActive);
+	$resupdateCourses = $update->execute($arrData);
+		return $resupdateCourses;
+	    
     }
 
 }
