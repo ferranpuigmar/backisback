0c9ad318aef33906bb9cf93f1924cf5e9bf5b600
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-09 19:44:33 +0200

    Update coursesModel.php

--- Models/coursesModel.php
+++ Models/coursesModel.php
@@ -2,7 +2,13 @@
 
 class coursesModel extends Mysql
 {
-   
+   	$this->strName = $name;
+	$this->strDescription = $description;
+	$this->strDate_start = $date_start;
+        $this->strDate_end = $date_end;
+        $this->strActive = $active; 
+         
+	
     public function __construct()
     {
         parent::__construct();
@@ -14,15 +20,27 @@ class coursesModel extends Mysql
         $reslistCourses = $this->select all($sql);  
            return $reslistCourses;
     }
+   // ALTA DE CURSOS                            
+    public function insertCourses(string $name, string $description, string $date_start, string $date_end, string $active)
+    {
+        
+         $sql = "INSERT INTO course(name,description,date_start,date_end, active) VALUES(?,?,?,?,?)";
+         $arrData = array($this->strName,$this->strDescription,$this->strDate_start,$this->strDate_end,$this->strActive);
+         $resinsertCourses = $this->insert($sql,$arrData);
+         return $resinsertCourses;
+
+    }
      // ACTUALIZAMOS ALGUNO DE LOS CAMPOS DE CURSOS 
   
     public function updateCourses(int $id_course, string $name,string $description,string $date_start,string $date_end, string $active)
     {
-        $this->strName = $name;
+        /*
+	$this->strName = $name;
 	$this->strDescription = $description;
 	$this->strDate_start = $date_start;
         $this->strDate_end = $date_end;
         $this->strActive = $active; 
+	*/
       
         $sql = "UPDATE courses SET name=?, description=?, date_start=?, date_end=?, active=? WHERE id_course=$id_course ";
         $arrData = array($this->strName,$this->srtDescription,$this->strDate_start,$this->srtDate_end,$this->strActive);
