b38e2923537197411c4f4c5ae98a4f5735e9ba7d
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-09 04:07:14 +0200

    Update calendarModel.php

--- Models/calendarModel.php
+++ Models/calendarModel.php
@@ -9,18 +9,37 @@ class calendarModel extends Mysql
   {
     parent::__construct();
   }
+  
+  //recuperamos el curso de un estudiante
+  
+    public function listCourses(string $user)
+  
+    {
+    $sql = "select cur.description, cur.date_start, cur.date_end
+            from students s JOIN enrollment en JOIN courses cur
+            where s.username = '$this->strUser' and
+            s.id = en.id_student                and 
+            en.id_course = cur.id_course";
+      
+    $request = $this->select_all($sql);
+      
+              return $request;
+    }
+  
   //recuperamos calendario de clases de un estudiante conectado
 
-  public function listSchedule(string $user)
+  public function listClass(string $user)
   {
-    $sql = "select en.id_course, cla.id_class, sch.day, sch.time_start, sch.time_end, concat_ws('  ', te.name, te.surname)
+    $sql = "select cla.name, sch.day, sch.time_start, sch.time_end, concat_ws(' ', te.name, te.surname)
                 from students s JOIN enrollment en JOIN class cla JOIN schedule sch JOIN teachers te
                 where s.username     = '$this->strUser'      and
                       s.id           = en.id_student         and  
                       en.id_course   = cla.id_course         and 
                       cla.id_class   = sch.id_class          and
                       cla.id_teacher = te.id_teacher";
+    
     $request = $this->select_all($sql);
-    return $request;
+    
+          return $request;
   }
 }
