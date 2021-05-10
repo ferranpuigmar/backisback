5a247afb7fbfc7c765398cc022b7772c7ee22bf2
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-10 19:20:08 +0200

    Update registerModel.php

--- Models/registerModel.php
+++ Models/registerModel.php
@@ -26,40 +26,6 @@ class registerModel extends Mysql
         return $request;
     }
 
-    public function registerStudent(string $username, string $pass, string $email, string $name, string $surname, string $telephone, string $nif)
-    {
-        $this->strUsername = $username;
-        $this->strPassword = $pass;
-        $this->strEmail = $email;
-        $this->strName = $name;
-        $this->strSurname = $surname;
-        $this->strTelephone = $telephone;
-        $this->strNif = $nif;
-        //ojo ver como recoger la fecha y hora en formato timestamp $this->???Date_registered= $date_registered;
-
-        //CONFIRMAMOS QUE NO EXISTA EL ESTUDIANTE QUE VAMOS A INSERTAR 
-        /*
-        $sql = "SELECT * from students 
-        WHERE username = '$this->strUsername'";
-        $request = $this->select($sql);
-        return $request;
-
-        if (empty($request)) {
-        */
-        //OJO FALTA LA FECHA 
-            $sql = "INSERT INTO students(username,pass,email,name,surname,telephone,nif,date_registered) VALUES(?,?,?,?,?,?,?,?)";
-            $insert = $this->prepare($sql);
-            $arrData = array($this->strUsername, $this->strPassword, $this->strEmail, $this->strEmail, $this->strName, $this->strSurname, $this->strTelephone, $this->strNif);
-            $resInsert = $insert->execute($arrData);
-            $request = $this->insert($sql, $arrData);
-            $request = $this->lastInsertId();
-            return $request; 
-        /*
-        } else {
-            return "exit";
-        }
-        */
-    }
      // ALTA DE CURSOS                            
     public function insertStudents(string $username, string $pass, string $email, string $name, string $surname, string $telephone, string $nif)
     {
