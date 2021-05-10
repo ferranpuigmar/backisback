3e4d7e40dce849f775c9f08783200a1fccfe6860
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-10 10:18:08 +0200

    Update coursesModel.php

--- Models/coursesModel.php
+++ Models/coursesModel.php
@@ -21,19 +21,27 @@ class coursesModel extends Mysql
            return $reslistCourses;
     }
    // ALTA DE CURSOS                            
-    public function insertCourses(string $name, string $description, string $date_start, string $date_end, string $active)
+    public function insertCourses(int $id_courses, string $name, string $description, string $date_start, string $date_end, string $active)
     {
+	$return ="";
+	$this->intId_courses = $id_courses;    
         $this->strName = $name;
 	$this->strDescription = $description;
 	$this->strDate_start = $date_start;
         $this->strDate_end = $date_end;
         $this->strActive = $active; 
 	    
-         $sql = "INSERT INTO courses(name,description,date_start,date_end, active) VALUES(?,?,?,?,?)";
-         $arrData = array($this->strName,$this->strDescription,$this->strDate_start,$this->strDate_end,$this->strActive);
-         $resinsertCourses = $this->insert($sql,$arrData);
-         return $resinsertCourses;
-
+	  $sql = "SELECT * from courses where id_courses = = '$this->intId_courses'";
+          $reslistCourses = $this->select_all($sql); 
+	  if (empty($reslistCourses)) {
+             $sql_insert = "INSERT INTO courses(name,description,date_start,date_end, active) VALUES(?,?,?,?,?)";
+             $arrData = array($this->strName,$this->strDescription,$this->strDate_start,$this->strDate_end,$this->strActive);
+             $resinsertCourses = $this->insert($sql_insert,$arrData);
+             $return = $resinsertCourses;
+	  }else{
+	     $return = "exits";
+	  }
+	    return $return;
     }
      // ACTUALIZAMOS CAMPOS DE LA TABLA CURSOS
   
