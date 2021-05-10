3e623b15d14ed80d12b0795ac0f80f52ec02a9f8
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-10 21:36:29 +0200

    Update coursesModel.php

--- Models/coursesModel.php
+++ Models/coursesModel.php
@@ -33,7 +33,7 @@ class coursesModel extends Mysql
         $this->strDate_end = $date_end;
         $this->strActive = $active;
 
-        $sql = "SELECT * from courses where id_course = '$this->intId_course'";
+        $sql = "SELECT * from courses where id_course = $this->intId_course";
         $reslistCourses = $this->select_all($sql);
         if (empty($reslistCourses)) {
             $sql_insert = "INSERT INTO courses(name,description,date_start,date_end, active) VALUES(?,?,?,?,?)";
@@ -56,59 +56,21 @@ class coursesModel extends Mysql
         $this->strDate_start = $date_start;
         $this->strDate_end = $date_end;
         $this->strActive = $active;
-
-        $sql = "SELECT * from courses where id_course = '$this->intId_course";
-        $reslistCourses = $this->select_all($sql);
-        // se comprueba que exista el curso 
-        if (empty($reslistCourses)) {
-            $return = "exits";
-        } else {
+       
             $sql_update = "UPDATE courses SET name=?, description=?, date_start=?, date_end=?, active=? WHERE id_course = $this->strId_course";
-            $arrData = array($this->strName, $this->srtDescription, $this->strDate_start, $this->srtDate_end, $this->strActive);
+            $arrData = array($this->strName, $this->strDescription, $this->strDate_start, $this->strDate_end, $this->strActive);
             $resupdateCourses = $this->update($sql_update, $arrData);
-            $return = $resupdateCourses;
-        }
-        return $return;
+            return $resupdateCourses;    
     }
 
     // BORRAMOS CURSOS
     public function deleteCourse(int $id_course)
     {
         $this->strId_course = $id_course;
-        $sql = "SELECT * from courses where id_course = $this->strId_course";
-        $reslistCourses = $this->select($sql);
-
-        // se comprueba que exista el curso
-        if (empty($reslistCourses)) {
-            $return = "exist";
-        } else {
+	
             $sql_delete = "DELETE from courses WHERE id_course= $this->strId_course";
             $resdeleteCourses = $this->delete($sql_delete);
             $return = $resdeleteCourses;
-        }
-        return $return;
+            return $resdeleteCourses;
     }
-
-    // public function updateCourses(int $id_course, string $name, string $description, string $date_start, string $date_end, string $active)
-    // {
-
-    //     $this->strName = $name;
-    //     $this->strDescription = $description;
-    //     $this->strDate_start = $date_start;
-    //     $this->strDate_end = $date_end;
-    //     $this->strActive = $active;
-
-    //     $sql = "UPDATE courses SET name=?, description=?, date_start=?, date_end=?, active=? WHERE id_course=$id_course ";
-    //     $arrData = array($this->strName, $this->srtDescription, $this->strDate_start, $this->srtDate_end, $this->strActive);
-    //     $resupdateCourses = $update->execute($arrData);
-    //     return $resupdateCourses;
-    // }
-    // // BORRAMOS CURSOS 
-
-    // public function deleteCourses(int $id_course)
-    // {
-    //     $sql = "DELETE from courses WHERE id_course=$id_course ";
-    //     $resdeleteCourses = $update->execute($arrData);
-    //     return $resdeleteCourses;
-    // }
 }
