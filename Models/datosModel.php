dabfe2d6e1b1235b79715945b31816c9e41ed167
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-12 21:44:50 +0200

    Update datosModel.php

--- Models/datosModel.php
+++ Models/datosModel.php
@@ -13,7 +13,7 @@ class datosModel extends Mysql
 
     //recuperamos usuarios o estudiantes 
 
-    public function listClass(string $strUser)
+    public function listDatos(string $strUser)
     {
         $this->strIdTeacher = $strUser;
         $this->strUser = $strUser;
@@ -28,5 +28,44 @@ class datosModel extends Mysql
 
         $request = $this->select_all($sql);
         return $request;
+    }
+    //recuperamos usuarios o estudiantes 
+
+    public function updateDatos(string $struser)
+    {
+        $this->strIdTeacher = $struser;
+        $this->strUser = $struser;
+
+        if ($_SESSION['is_admin'] == "1") {
+            $sql = "select *            from users_admin 
+            where username = $this->strUser";
+        } else {
+            $$sql = "select *            from students 
+            where username = $this->strUser";
+        }
+
+        $request = $this->select_all($sql);
+        return $request;
+    // ACTUALIZAMOS CAMPOS DE LA TABLA CURSOS
+    public function updateDatos(string $strusername, string $pass, string $mail)
+    {
+        $return = "";
+        $this->strUser = $strusername;
+        $this->strPasword = $pass;
+        $this->strMail = $mail;
+        
+        if ($_SESSION['is_admin'] == "1") {
+         $sql_update = "UPDATE users_admin  SET username=?, pass=?, mail=? where username = $this->strUser";
+         $arrData = array($this->strUser, $this->strPasword, $this->strMail);
+         $resupdateDatos = $this->update($sql_update, $arrData);
+         $result = 
+        }else{
+         $sql_update = "UPDATE users  SET username=?, pass=?, mail=? where username = $this->strUser";
+         $arrData = array($this->strUser, $this->strPasword, $this->strMail);
+         $resupdateDatos = $this->update($sql_update, $arrData);
+        
+        }
+        return $resupdate Datos;
+    
     }
 }
