5cc3c337b871ae12489105a51e64ebaad5244ea3
amorrondo <82721448+amorrondo@users.noreply.github.com>
2021-05-12 21:49:38 +0200

    Update datosModel.php

--- Models/datosModel.php
+++ Models/datosModel.php
@@ -29,23 +29,7 @@ class datosModel extends Mysql
         $request = $this->select_all($sql);
         return $request;
     }
-    //recuperamos usuarios o estudiantes 
-
-    public function updateDatos(string $struser)
-    {
-        $this->strIdTeacher = $struser;
-        $this->strUser = $struser;
-
-        if ($_SESSION['is_admin'] == "1") {
-            $sql = "select *            from users_admin 
-            where username = $this->strUser";
-        } else {
-            $$sql = "select *            from students 
-            where username = $this->strUser";
-        }
-
-        $request = $this->select_all($sql);
-        return $request;
+   
     // ACTUALIZAMOS CAMPOS DE LA TABLA CURSOS
     public function updateDatos(string $strusername, string $pass, string $mail)
     {
@@ -55,17 +39,17 @@ class datosModel extends Mysql
         $this->strMail = $mail;
         
         if ($_SESSION['is_admin'] == "1") {
-         $sql_update = "UPDATE users_admin  SET username=?, pass=?, mail=? where username = $this->strUser";
+         $sql_update = "UPDATE users_admin  SET username=?, password=?, mail=? where username = $this->strUser";
          $arrData = array($this->strUser, $this->strPasword, $this->strMail);
          $resupdateDatos = $this->update($sql_update, $arrData);
-         $result = 
+         $result = $resupdateDatos
         }else{
          $sql_update = "UPDATE users  SET username=?, pass=?, mail=? where username = $this->strUser";
          $arrData = array($this->strUser, $this->strPasword, $this->strMail);
          $resupdateDatos = $this->update($sql_update, $arrData);
+         $result = $resupdateDatos
         
         }
-        return $resupdate Datos;
-    
+        return $resupdateDatos;
     }
 }
